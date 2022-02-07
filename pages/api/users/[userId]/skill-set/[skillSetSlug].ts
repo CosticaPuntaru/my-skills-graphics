c53e10577638558f5b527d65ff1d40ac0eec07a2
costica.puntaru <costica.puntaru@everymatrix.com>
2022-02-08 01:31:17 +0200

    it has a graph

--- pages/api/users/[userId]/skill-set/[skillSetSlug].ts
+++ pages/api/users/[userId]/skill-set/[skillSetSlug].ts
@@ -61,13 +61,13 @@ const db: any = {
 };
 
 function stringToColour(str: string): string {
-    var hash = 0;
+    let hash = 0;
     for (let i = 0; i < str.length; i++) {
         hash = str.charCodeAt(i) + ((hash << 5) - hash);
     }
-    var colour = '#';
-    for (var i = 0; i < 3; i++) {
-        var value = (hash >> (i * 8)) & 0xFF;
+    let colour = '#';
+    for (let i = 0; i < 3; i++) {
+        const value = (hash >> (i * 8)) & 0xFF;
         colour += ('00' + value.toString(16)).substr(-2);
     }
     return colour;
